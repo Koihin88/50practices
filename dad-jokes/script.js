666fa33d210681cda7b3ad71316ead94fa8e8b01
Koihin Wong <koihinwong@Koihins-MacBook-Pro.local>
2022-09-08 10:04:41 +0800

    update

---
+++ dad-jokes/script.js
@@ -0,0 +1,18 @@
+const jokeElement = document.getElementById("joke");
+const jokeButton = document.getElementById("jokeBtn");
+
+jokeButton.addEventListener("click", generateJoke);
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  console.log(data.joke);
+  jokeElement.innerHTML = data.joke;
+}
